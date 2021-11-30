ea7fea9e1878281d811dd9676aeef443640bac38
a2uuz <aziznageeye9@gmail.com>
2021-11-30 22:21:18 +0300

    2016 collection wll continoue adding code snipets to it....

---
+++ 2016 collection.js
@@ -0,0 +1,22 @@
+var name = prompt("Sheeg magacaga wara NIGGA?");
+alert(`Asalamucalaykum, ${name}. maad jeceshsahay inad wax khatara aad aragto`);
+document.querySelector('html').innerHTML = `
+  <h1>ku soo dhawoow website ka ${name} yaa niggaa</h1>
+  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif">
+`;
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+const name = prompt("what is your name?");
+const message = "damn you look great ";
+alert (message + name);
+const thanks = prompt();
+const compliment = "can i have a picture with you?"
+alert(compliment);
+const sure = prompt();
+const img = document.createElement("img");
+img.src = "https://i.pinimg.com/474x/16/be/50/16be505bb738d3d822e0664f25f724d3.jpg";
+document.body.appendChild(img);
